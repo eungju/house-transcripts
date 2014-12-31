460a086310a7af9a48f1896a4813b8d999d6ee3f
Park Eungju <eungju@gmail.com>
2014-12-31 22:36:50 +0900

    Replace hpricot with nokogiri.

--- download.rb
+++ download.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'hpricot'
+require 'nokogiri'
 require 'open-uri'
 
 INDEX_PAGE = "http://community.livejournal.com/clinic_duty/12225.html"
 
-(open(INDEX_PAGE) { |f| Hpricot(f) }/"table > tbody > tr > td > a").each do |a|
+(open(INDEX_PAGE) { |f| Nokogiri::HTML(f) }.css("table > tbody > tr > td > a")).each do |a|
   #Skip season links
-  next if (a/"b").any?
+  next if (a.css("b")).any?
 
   #Download a transcript
   season, episode = a.previous.previous.inner_text.strip.split(".")
   title = a.inner_text.strip
   url = a["href"]
   puts "Download %s.%s %s" % [season, episode, title]
-  transcript = (open(url) { |f| Hpricot(f) }/"body div#content-wrapper div.b-singlepost > div")[2].inner_html
+  transcript = (open(url) { |f| Nokogiri::HTML(f) }.css("div.entryText"))[0].inner_html
   File.open("#{season}-#{episode}.html", "w") { |f|
     f.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body>')
     f.write(transcript)
